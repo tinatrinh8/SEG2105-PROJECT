7580cdd9f70be2afc4c88a6a0b17016b9affc92f
Tina Trinh <77304784+tinatrinh8@users.noreply.github.com>
2021-10-20 17:27:51 -0400

    Create README.md

--- Project/app/src/main/java/com/example/gymapp/AdminActivity.java
+++ Project/app/src/main/java/com/example/gymapp/AdminActivity.java
@@ -14,7 +14,7 @@ public class AdminActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
+        setContentView(R.layout.activity_main);
     }
 
 }
